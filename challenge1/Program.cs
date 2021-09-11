f07163b840e83a36b7e751bec9722ed353d68950
Rodrigo <robenoliel@gmail.com>
2021-09-11 13:29:40 -0300

    feat: challenge 1 completed

---
+++ challenge1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace challenge1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3; int b = 5; //dummy values
+            Console.WriteLine("Initial values: a = {0}, b = {1}", a, b);
+
+            a = a + b; //a = (a+b)
+            b = a - b; //b = (a+b)-b = a
+            a = a - b; //a = (a+b)-a = b
+
+            Console.WriteLine("Final values: a = {0}, b = {1}", a, b);
+        }
+    }
+}
